5bfb491be653a245a4e6ab8c655fcf80da155a9e
tnj <kitakore@gmail.com>
2014-04-17 00:35:52 +0900

    restore text and set color to gray

--- twist.rb
+++ twist.rb
@@ -28,22 +28,7 @@ EM.run do
     user = result['user']
     unless track_keywords.include?(user['screen_name'])
       status_url = "https://twitter.com/#{user['screen_name']}/status/#{result['id']}"
-      status_message = <<-"EOS"
-<table>
-  <tr>
-    <td valign="top">
-      <a href="#{status_url}"><img src="#{user['profile_image_url']}" width="48" height="48"></a>
-    </td>
-    <td>
-      <p>#{result['text']}</p>
-      <font color="#666">- <a href="#{user['url']}">@#{user['screen_name']}</a>
-      (#{user['name']} / #{user['followers_count']} followers)
-      via #{result['source']}</font>
-    </td>
-  </tr>
-</table>
-      EOS
-      hipchat_client[ENV['HIPCHAT_ROOM_NAME']].send(ENV['HIPCHAT_SENDER_NAME'], status_message, message_format: 'html')
+      hipchat_client[ENV['HIPCHAT_ROOM_NAME']].send(ENV['HIPCHAT_SENDER_NAME'], status_url, message_format: 'text', color: 'gray')
     end
   end
 end
