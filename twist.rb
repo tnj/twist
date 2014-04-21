2358adfa2bd5a4901606c6618462da62e63f6086
tnj <kitakore@gmail.com>
2014-04-21 16:50:31 +0900

    filter RT

--- twist.rb
+++ twist.rb
@@ -22,18 +22,17 @@ options = {
 EM.run do
   twitter_client = EM::Twitter::Client.connect(options)
   hipchat_client = HipChat::Client.new(ENV['HIPCHAT_API_TOKEN'])
-
   twitter_client.each do |result|
     result = JSON.parse(result)
     user = result['user']
     status_url = "https://twitter.com/#{user['screen_name']}/status/#{result['id']}"
     if result['retweeted_status']
-      rt = result['retweeted_status']
-      if rt['text'] != result['text']
-        hipchat_client[ENV['HIPCHAT_ROOM_NAME']].send(ENV['HIPCHAT_SENDER_NAME'], status_url, message_format: 'text', color: 'gray')
+      if result['in_reply_to_status_id']
+        hipchat_client[ENV['HIPCHAT_ROOM_NAME']].send('Twitter RT', status_url, message_format: 'text', color: 'gray')
       end
+      rt = result['retweeted_status']
       if rt['retweet_count'] % 5 == 0
-        hipchat_client[ENV['HIPCHAT_ROOM_NAME']].send('RT', <<"EOT", message_format: 'html', color: 'yellow')
+        hipchat_client[ENV['HIPCHAT_ROOM_NAME']].send('Twitter RT', <<"EOT", message_format: 'html', color: 'yellow')
 <b>#{result['retweeted_status']['retweet_count']} retweets</b>: #{rt['text'].length > 30 ? rt['text'].slice(0,30) + '...' : rt['text']} - @<a href="#{status_url}">#{rt['user']['screen_name']}</a>
 EOT
       end
